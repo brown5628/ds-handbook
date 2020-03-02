ec9206249e06d10a27da91cc689af174a5a2e913
brown5628 <brown5628@gmail.com>
2020-03-02 07:42:08 -0500

    Continue ch 2

--- ch_2_intro_to_numpy.py
+++ ch_2_intro_to_numpy.py
@@ -1,7 +1,12 @@
 # %%
 import numpy as np
 import array
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+from scipy import special
 
+seaborn.set()
 # %%
 result = 0
 for i in range(100):
@@ -360,5 +365,274 @@ print("log10(x) =", np.log10(x))
 x = [0, 0.001, 0.01, 0.1]
 print("exp(x) - 1 =", np.expm1(x))
 print("log(1 + x) =", np.log1p(x))
+
+# %%
+x = [1, 5, 10]
+print("gamma(x) =", special.gamma(x))
+print("ln|gamma(x) = ", special.gammaln(x))
+print("beta(x, 2) =", special.beta(x, 2))
+
+# %%
+x = np.array([0, 0.3, 0.7, 1.0])
+print("erf(x) =", special.erf(x))
+print("erfc(x) =", special.erfc(x))
+print("erfinv(x) =", special.erfinv(x))
+
+# %%
+x = np.arange(5)
+y = np.empty(5)
+np.multiply(x, 10, out=y)
+print(y)
+
+# %%
+y = np.zeros(10)
+np.power(2, x, out=y[::2])
+print(y)
+
+# %%
+x = np.arange(1, 6)
+np.add.reduce(x)
+
+# %%
+np.multiply.reduce(x)
+
+# %%
+np.add.accumulate(x)
+
+# %%
+np.multiply.accumulate(x)
+
+# %%
+x = np.arange(1, 6)
+np.multiply.outer(x, x)
+
+# %%
+L = np.random.random(100)
+sum(L)
+
+# %%
+np.sum(L)
+
+# %%
+big_array = np.random.rand(1000000)
+min(big_array), max(big_array)
+
+# %%
+np.min(big_array), np.max(big_array)
+
+# %%
+print(big_array.min(), big_array.max(), big_array.sum())
+
+# %%
+M = np.random.random((3, 4))
+print(M)
+
+# %%
+M.sum()
+
+# %%
+M.min(axis=0)
+
+# %%
+M.max(axis=1)
+
+# %%
+data = pd.read_csv("data/president_heights.csv")
+heights = np.array(data["height(cm)"])
+print(heights)
+
+# %%
+print("Mean height:", heights.mean())
+print("Standard deviation:", heights.std())
+print("Minimum height:", heights.min())
+print("Maximum height:", heights.max())
+
+
+# %%
+print("25th percentile:", np.percentile(heights, 25))
+print("Median:", np.median(heights))
+print("75th percentile:", np.percentile(heights, 75))
+
+# %%
+plt.hist(heights)
+plt.title("Height Distribution of US Presidents")
+plt.xlabel("height (cm)")
+plt.ylabel("number")
+
+# %%
+a = np.array([0, 1, 2])
+b = np.array([5, 5, 5])
+a + b
+
+# %%
+a + 5
+
+# %%
+M = np.ones((3, 3))
+M
+
+# %%
+M + a
+
+# %%
+a = np.arange(3)
+b = np.arange(3)[:, np.newaxis]
+
+print(a)
+print(b)
+
+# %%
+a + b
+
+# %%
+M = np.ones((2, 3))
+a = np.arange(3)
+
+# %%
+M + a
+
+# %%
+a = np.arange(3).reshape((3, 1))
+b = np.arange(3)
+
+# %%
+a + b
+
+# %%
+M = np.ones((3, 2))
+a = np.arange(3)
+
+
+# %%
+# M + a
+
+# %%
+a[:, np.newaxis].shape
+
+M + a[:, np.newaxis]
+
+
+# %%
+np.logaddexp(M, a[:, np.newaxis])
+
+# %%
+X = np.random.random((10, 3))
+
+
+# %%
+Xmean = X.mean(0)
+Xmean
+
+# %%
+X_centered = X - Xmean
+
+
+# %%
+X_centered.mean(0)
+
+# %%
+x = np.linspace(0, 5, 50)
+y = np.linspace(0, 5, 50)[:, np.newaxis]
+
+z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+
+# %%
+plt.imshow(z, origin="lower", extent=[0, 5, 0, 5], cmap="magma")
+plt.colorbar()
+
+# %%
+rainfall = pd.read_csv("data/Seattle2014.csv")["PRCP"].values
+inches = rainfall / 254
+inches.shape
+
+# %%
+plt.hist(inches, 40)
+
+# %%
+x = np.array([1, 2, 3, 4, 5])
+x < 3
+
+# %%
+x > 3
+
+# %%
+x <= 3
+
+# %%
+x >= 3
+
+# %%
+x != 3
+
+# %%
+x == 3
+
+# %%
+(2 * x) == (x ** 2)
+
+# %%
+rng = np.random.RandomState(0)
+x = rng.randint(10, size=(3, 4))
+x
+
+
+# %%
+x < 6
+
+# %%
+print(x)
+
+# %%
+np.count_nonzero(x < 6)
+
+# %%
+np.sum(x < 6)
+
+# %%
+np.sum(x < 6, axis=1)
+
+# %%
+np.any(x > 6)
+
+# %%
+np.any(x < 0)
+
+# %%
+np.all(x < 10)
+
+# %%
+np.all(x == 6)
+
+# %%
+np.all(x < 8, axis=1)
+
+# %%
+np.sum((inches > 0.5) & (inches < 1))
+
+# %%
+np.sum(~((inches <= 0.5) | (inches >= 1)))
+
+# %%
+print("Number days without rain:", np.sum(inches == 0))
+print("Number days with rain:", np.sum(inches != 0))
+print("Days with more than .5 inches:", np.sum(inches > 0.5))
+print("Rainy days with < .1 inches:", np.sum((inches > 0) & (inches < 0.2)))
+
+# %%
+x[x < 5]
+
+# %%
+rainy = inches > 0
+
+summer = (np.arange(365) - 172 < 90) & (np.arange(365) - 172 > 0)
+
+print("Median precip on rainy days in 2014 (inches):", np.median(inches[rainy]))
+print("median precip on summer days in 2014 (inches): ", np.median(inches[summer]))
+print("Maximum precip on summer days in 2014 (inches):", np.max(inches[summer]))
+print(
+    "Median precip on non-summer rainy days (inches):",
+    np.median(inches[rainy & ~summer]),
+)
 
 # %%
