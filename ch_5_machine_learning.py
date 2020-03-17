641e49f1360622372f20cbff7b2e70631f88790e
brown5628 <brown5628@gmail.com>
2020-03-17 07:30:08 -0400

    Ch 5- complete Random Forests

--- ch_5_machine_learning.py
+++ ch_5_machine_learning.py
@@ -865,6 +865,104 @@ plt.ylabel("predicted label")
 
 # %%
 X, y = make_blobs(n_samples=300, centers=4, random_state=0, cluster_std=1)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap="autumn")
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap="rainbow")
 
 # %%
+tree = DecisionTreeClassifier().fit(X, y)
+
+# %%
+
+
+def visualize_classifier(model, X, y, ax=None, cmap="rainbow"):
+    ax = ax or plt.gca()
+
+    ax.scatter(
+        X[:, 0], X[:, 1], c=y, s=30, cmap=cmap, clim=(y.min(), y.max()), zorder=3
+    )
+
+    ax.axis("tight")
+    ax.axis("off")
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    model.fit(X, y)
+    xx, yy = np.meshgrid(np.linspace(*xlim, num=200), np.linspace(*ylim, num=200))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    n_classes = len(np.unique(y))
+    contours = ax.contourf(
+        xx,
+        yy,
+        Z,
+        alpha=0.3,
+        levels=np.arange(n_classes + 1) - 0.5,
+        cmap=cmap,
+        clim=(y.min(), y.max()),
+        zorder=1,
+    )
+
+    ax.set(xlim=xlim, ylim=ylim)
+
+
+# %%
+visualize_classifier(DecisionTreeClassifier(), X, y)
+
+# %%
+tree = DecisionTreeClassifier()
+bag = BaggingClassifier(tree, n_estimators=100, max_samples=0.8, random_state=1)
+
+bag.fit(X, y)
+visualize_classifier(bag, X, y)
+
+# %%
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+visualize_classifier(model, X, y)
+
+# %%
+rng = np.random.RandomState(42)
+x = 10 * rng.rand(200)
+
+
+def model(x, sigma=0.3):
+    fast_oscillation = np.sin(5 * x)
+    slow_oscillation = np.sin(0.5 * x)
+    noise = sigma * rng.randn(len(x))
+
+    return slow_oscillation + fast_oscillation + noise
+
+
+y = model(x)
+plt.errorbar(x, y, 0.3, fmt="o")
+
+# %%
+forest = RandomForestRegressor(200)
+forest.fit(x[:, None], y)
+
+xfit = np.linspace(0, 10, 1000)
+yfit = forest.predict(xfit[:, None])
+ytrue = model(xfit, sigma=0)
+
+plt.errorbar(x, y, 0.3, fmt="o", alpha=0.5)
+plt.plot(xfit, yfit, "-r")
+plt.plot(xfit, ytrue, "-k", alpha=0.5)
+
+# %%
+digits = load_digits()
+digits.keys()
+
+# %%
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    digits.data, digits.target, random_state=0
+)
+model = RandomForestClassifier(n_estimators=1000)
+model.fit(Xtrain, ytrain)
+ypred = model.predict(Xtest)
+
+# %%
+print(classification_report(ypred, ytest))
+
+# %%
+mat = confusion_matrix(ytest, ypred)
+sns.heatmap(mat.T, square=True, annot=True, fmt="d", cbar=False)
+plt.xlabel("true label")
+plt.ylabel("predicted label")
