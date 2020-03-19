2d7d588c04d10fd0cc9f12f320e6f00656d0f31a
brown5628 <brown5628@gmail.com>
2020-03-19 08:08:30 -0400

    Ch 5- Complete Gaussian Mixture Models

--- ch_5_machine_learning.py
+++ ch_5_machine_learning.py
@@ -55,6 +55,8 @@ from scipy.stats import mode
 from sklearn.manifold import TSNE
 from sklearn.datasets import load_sample_image
 from sklearn.cluster import MiniBatchKMeans
+from scipy.spatial.distance import cdist
+from matplotlib.patches import Ellipse
 
 # %%
 iris = sns.load_dataset("iris")
@@ -1482,5 +1484,167 @@ ax[0].imshow(china)
 ax[0].set_title("Original Image", size=16)
 ax[1].imshow(china_recolored)
 ax[1].set_title("16-color Image", size=16)
+
+# %%
+X, y_true = make_blobs(n_samples=400, centers=4, cluster_std=0.6, random_state=0)
+X = X[:, ::-1]
+
+# %%
+kmeans = KMeans(4, random_state=0)
+labels = kmeans.fit(X).predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap="viridis")
+
+# %%
+
+
+def plot_kmeans(kmeans, X, n_clusters=4, rseed=0, ax=None):
+    labels = kmeans.fit_predict(X)
+
+    ax = ax or plt.gca()
+    ax.axis("equal")
+    ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap="viridis", zorder=2)
+
+    centers = kmeans.cluster_centers_
+    radii = [cdist(X[labels == i], [center]).max() for i, center in enumerate(centers)]
+    for c, r in zip(centers, radii):
+        ax.add_patch(plt.Circle(c, r, fc="#CCCCCC", lw=3, alpha=0.5, zorder=1))
+
+
+# %%
+kmeans = KMeans(n_clusters=4, random_state=0)
+plot_kmeans(kmeans, X)
+
+# %%
+rng = np.random.RandomState(13)
+X_stretched = np.dot(X, rng.randn(2, 2))
+
+kmeans = KMeans(n_clusters=4, random_state=0)
+plot_kmeans(kmeans, X_stretched)
+
+# %%
+gmm = GaussianMixture(n_components=4).fit(X)
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap="viridis")
+
+# %%
+probs = gmm.predict_proba(X)
+print(probs[:5].round(3))
+
+# %%
+size = 50 * probs.max(1) ** 2
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap="viridis", s=size)
+
+# %%
+
+
+def draw_ellipse(position, covariance, ax=None, **kwargs):
+    """Draw an ellipse with a given position and covariance"""
+    ax = ax or plt.gca()
+
+    if covariance.shape == (2, 2):
+        U, s, Vt = np.linalg.svd(covariance)
+        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width, height = 2 * np.sqrt(s)
+    else:
+        angle = 0
+        width, height = 2 * np.sqrt(covariance)
+
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position, nsig * width, nsig * height, angle, **kwargs))
+
+
+def plot_gmm(gmm, X, label=True, ax=None):
+    ax = ax or plt.gca()
+    labels = gmm.fit(X).predict(X)
+    if label:
+        ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap="viridis", zorder=2)
+    else:
+        ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, zorder=2)
+    ax.axis("equal")
+
+    w_factor = 0.2 / gmm.weights_.max()
+    for pos, covar, w in zip(gmm.means_, gmm.covariances_, gmm.weights_):
+        draw_ellipse(pos, covar, alpha=w * w_factor)
+
+
+# %%
+gmm = GaussianMixture(n_components=4, random_state=42)
+plot_gmm(gmm, X)
+
+# %%
+gmm = GaussianMixture(n_components=4, covariance_type="full", random_state=42)
+plot_gmm(gmm, X_stretched)
+
+# %%
+Xmoon, ymoon = make_moons(200, noise=0.05, random_state=0)
+plt.scatter(Xmoon[:, 0], Xmoon[:, 1])
+
+# %%
+gmm2 = GaussianMixture(n_components=2, covariance_type="full", random_state=0)
+plot_gmm(gmm2, Xmoon)
+
+# %%
+gmm16 = GaussianMixture(n_components=16, covariance_type="full", random_state=0)
+plot_gmm(gmm16, Xmoon, label=False)
+
+# %%
+# Xnew = gmm16.sample(400)
+# plt.scatter(Xnew[:, 0], Xnew[:, 1])
+
+# %%
+n_components = np.arange(1, 21)
+models = [
+    GaussianMixture(n, covariance_type="full", random_state=0).fit(Xmoon)
+    for n in n_components
+]
+
+plt.plot(n_components, [m.bic(Xmoon) for m in models], label="BIC")
+plt.plot(n_components, [m.aic(Xmoon) for m in models], label="AIC")
+plt.legend(loc="best")
+plt.xlabel("n_components")
+
+# %%
+digits = load_digits()
+digits.data.shape
+
+# %%
+
+
+def plot_digits(data):
+    fig, ax = plt.subplots(
+        10, 10, figsize=(8, 8), subplot_kw=dict(xticks=[], yticks=[])
+    )
+    fig.subplots_adjust(hspace=0.05, wspace=0.05)
+    for i, axi in enumerate(ax.flat):
+        im = axi.imshow(data[i].reshape(8, 8), cmap="binary")
+        im.set_clim(0, 16)
+    plot_digits(digits.data)
+
+
+# %%
+pca = PCA(0.99, whiten=True)
+data = pca.fit_transform(digits.data)
+data.shape
+
+# %%
+n_components = np.arange(50, 210, 10)
+models = [
+    GaussianMixture(n, covariance_type="full", random_state=0) for n in n_components
+]
+aics = [model.fit(data).aic(data) for model in models]
+plt.plot(n_components, aics)
+
+# %%
+gmm = GaussianMixture(110, covariance_type="full", random_state=0)
+gmm.fit(data)
+print(gmm.converged_)
+
+# %%
+data_new = gmm.sample(n_samples=100)
+data_new[0].shape
+
+# %%
+digit_new = pca.inverse_transform(data_new[0])
+plot_digits(digit_new)
 
 # %%
